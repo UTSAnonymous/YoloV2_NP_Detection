8bdff86bcc1b1b2669c73c2e9f03f96f6b55e8e4
UTSAnonymous <12480000@student.uts.edu.au>
2019-05-16 00:56:25 +1000

    CNN created, training required

---
+++ creating_dataset.m
@@ -0,0 +1,74 @@
+
+% code for creating the .mat file for training, validation and testing
+% dataset
+
+%% Create a matlab .mat file for our dataset 
+
+path_to_data ='/media/nicholas/SSD/Uni/2019/Autumn/Neural Networks and Fuzzy Logic/Final Project/UFPR-ALPR dataset/testing';
+
+imds = imageDatastore( path_to_data,'IncludeSubfolders',true);
+imageFilename = imds.Files;
+
+
+%% read images in the imds
+% img = readimage(imds,1);
+% 
+% I = insertShape(img,'Rectangle',[911 525 73 29]);
+% 
+% imshow(I)
+
+
+%% Creating all the bounding box ground truth into an array
+
+row = size(imds.Files, 1);
+numplate = cell(row, 1);
+
+for i = 1:row
+    
+% get the path to the image
+str = imds.Files{i};
+
+% change the str from .png to .txt
+labels = strrep(str, '.png','.txt');
+
+% read the file
+filetext = fileread(labels);
+
+% get the number plate line
+expr = '[^\n]*position_plate[^\n]*';
+matches = regexp(filetext,expr,'match');
+
+% remove all str in the char
+bounding_box_str = erase(matches,'position_plate: ');
+
+% convert char into a 1x4 matrix
+bounding_box = str2num(bounding_box_str{1});
+
+numplate{i,1} = bounding_box;
+end
+
+size(numplate)
+
+
+%% create a table and save the data
+
+numberplate_dataset = table(imageFilename, numplate)
+
+save numplateTestDataset numberplate_dataset
+
+summary(numberplate_dataset)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ rcnn_yolo.m
@@ -0,0 +1,53 @@
+
+%% Creating the network for training 
+
+% parameters of creating the yolov2 detection r-cnn
+imageSize = [224 224 3];
+numClasses = 1;
+
+% anchor box size is determine using the estimating_boundingbox script
+anchorBoxes = [49 36;
+               67 23;
+               138 50;
+               98 34];
+
+% use alexnet feature extraction 
+network = resnet50();
+featureLayer = 'activation_49_relu';
+
+% create a new lgraph of alexnet feature extraction and yolov2 r-cnn object
+% detection
+lgraph = yolov2Layers(imageSize,numClasses,anchorBoxes,network,featureLayer);
+
+% show lgraph
+%analyzeNetwork(lgraph)
+
+%% get data from the dataset
+
+% get the 3 data (training, validation and testing)
+train_data = load('numplateTrainingDataset.mat');
+val_data = load('numplateValDataset.mat');
+test_data = load('numplateTestDataset.mat');
+
+trainds = train_data.numberplate_dataset;
+valds = val_data.numberplate_dataset;
+testds = test_data.numberplate_dataset;
+
+
+%% Training of the data 
+
+% TRAINING OPTION/ SETTINGS
+%   - sgdm = stochastic gradient descent
+%   - Batch size = 10
+%   - Initial Learning Rate = 1e-3
+%   - Shuffle = shuffle the data to distribute it
+%   - Validation Data = prevent overfitting
+
+options = trainingOptions('sgdm', ...
+    'MiniBatchSize', 16, ...
+    'InitialLearnRate', 1e-3, ...
+    'MaxEpochs',10,...
+    'Shuffle','every-epoch');
+
+% Train yolo v2 detector
+[detector,info] = trainYOLOv2ObjectDetector(trainds,lgraph,options)
